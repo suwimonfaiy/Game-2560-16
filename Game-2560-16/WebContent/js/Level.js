1bbc146aba98f17aaabd1a43578c3004bcdaeb83
Suwimon <Suwimon@10.61.235.205>
2017-11-15 01:01:41 +0700

    แก้ไขไฟล์2

--- Game-2560-16/WebContent/js/Level.js
+++ Game-2560-16/WebContent/js/Level.js
@@ -18,53 +18,30 @@ Level.prototype.create = function() {
 	this.map = this.game.add.tilemap("lab7");
 	this.map.addTilesetImage('titleset');
 	this.maplayer = this.map.createLayer("Tile Layer 1");
+	this.cat9 = this.addCat(280, 300);
+	this.cat9.play("jamedead");
+	
+};
+function gframes(key,n){
+	var f=[ ];
+	for(var i=0;i<=n;i++){
+	 var kf=key+"_"+(("00" + i).slice (-3));
+	 f.push(kf);
+	}
+	return f;
+	}
+Level.prototype.addCat = function(x, y) {
+	var c = this.add.sprite(x, y, "jamedead");
+	c.animations.add("jamedead", gframe("jamedead", 10), 12, true);
+	
+	c.play("jamedead");
+	c.anchor.set(0.5, 1);
+	this.game.physics.enable(c);
+	c.body.collideWorldBounds = true;
+	return c;
 };
 
-Level.prototype.addMonkey = function() {
-	// add monkey
-	this.monkey = this.add.sprite(this.world.centerX, this.world.height - 250,
-			"mono");
-	this.monkey.anchor.set(0.5, 0.5);
 
-	// listen for a monkey click
-	this.monkey.inputEnabled = true;
-	this.monkey.events.onInputDown.add(this.hitMonkey, this);
-};
-
-Level.prototype.moveMonkey = function() {
-	// tween monkey like a yoyo
-	var twn = this.add.tween(this.monkey);
-	twn.to({
-		y : 200
-	}, 1000, "Quad.easeInOut", true, 0, Number.MAX_VALUE, true);
-
-	// rotate monkey
-	twn = this.add.tween(this.monkey);
-	twn.to({
-		angle : 360
-	}, 2000, "Linear", true, 0, Number.MAX_VALUE);
-};
-
-Level.prototype.hitMonkey = function() {
-	// stop all monkey's movements
-	this.tweens.removeAll();
-
-	// rotate monkey
-	var twn = this.add.tween(this.monkey);
-	twn.to({
-		angle : this.monkey.angle + 360
-	}, 1000, "Linear", true);
-
-	// scale monkey
-	twn = this.add.tween(this.monkey.scale);
-	twn.to({
-		x : 0.1,
-		y : 0.1
-	}, 1000, "Linear", true);
-
-	// when tween completes, quit the game
-	twn.onComplete.addOnce(this.quitGame, this);
-};
 
 Level.prototype.quitGame = function() {
 	this.game.state.start("Menu");
