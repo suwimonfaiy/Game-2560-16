40e17e4a6a5b0c33a435874dcc34b280acd1cfa9
Suwimon <Suwimon@10.61.232.148>
2017-11-17 00:42:36 +0700

    แก้ไข จริงจัง

--- Game-2560-16/WebContent/js/Main.js
+++ Game-2560-16/WebContent/js/Main.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	// Create your Phaser game and inject it into an auto-created canvas.
 	// We did it in a window.onload event, but you can do it anywhere (requireJS
 	// load, anonymous function, jQuery dom ready, - whatever floats your boat)
-	var game = new Phaser.Game(1200, 800, Phaser.AUTO);
+	var game = new Phaser.Game(800, 500, Phaser.AUTO);
 
 	// Add the States your game has.
 	game.state.add("Boot", Boot);
--- Game-2560-16/WebContent/js/Menu.js
+++ Game-2560-16/WebContent/js/Menu.js
@@ -19,15 +19,23 @@ Menu.prototype.create = function() {
 	this.bg.height = this.game.height;
 	
 	var logo = this.add.sprite(this.world.centerX,250,"name");
-	logo.width = 600;
-	logo.anchor.set(0.5,0.5);
+	logo.anchor.set(0.5,1.5);
 	
 	
 	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
-	"start");
+	"startt");
 	sprite.anchor.set(0.5, 0.5);
-	
 	this.input.onDown.add(this.startGame, this);
+	
+	var sprite1 = this.add.sprite(this.world.centerX, this.world.centerY,
+	"about");
+	sprite1.anchor.set(0.5, -1);
+	var sprite2 = this.add.sprite(this.world.centerX, this.world.centerY,
+	"exit");
+	sprite2.anchor.set(0.5, -2);
+	
+	
+	
 };
 
 Menu.prototype.startGame = function() {
